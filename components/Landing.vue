354e6180e12991e65dded10f3d502dbc067fd08c
Amrani Houssain <amranidev@gmail.com>
2017-03-15 23:29:11 +0000

    update Landing

--- components/Landing.vue
+++ components/Landing.vue
@@ -23,7 +23,7 @@
 export default {
     mounted() {
         this.$auth.state({
-            forward: '/app',
+            forward: '/',
             redirect: '/login',
             then: (user) =>{
 
