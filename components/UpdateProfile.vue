89611f869152c07bfe5cc03c29e29cf2209037ff
Amrani Houssain <amranidev@gmail.com>
2017-03-17 18:51:34 +0000

    update profile

--- components/UpdateProfile.vue
+++ components/UpdateProfile.vue
@@ -1,4 +1,6 @@
 <template>
+<div>
+<navbar></navbar>
 	<div class="col-md-8 col-md-offset-2">
 		<div class="push-down"></div>
 		<div class="panel panel-primary">
@@ -17,10 +19,14 @@
 			</div>
 		</div>
 	</div>
+</div>
 </template>
 
 
 <script>
+
+import navbar from './../components/partials/Navbar.vue'
+
 export default {
     mounted() {
         console.log("Component mounted")
@@ -29,7 +35,8 @@ export default {
             rederict: '/login',
             then: (user) => {
                 this.userEmail = this.$auth.user().email
-				 this.storage.getDownloadURL({
+                this.userName = user.displayName
+				this.$storage.getDownloadURL({
             		ref: this.$auth.user().photoURL,
             		result: (url) => {
             			this.profilePicture = url
@@ -57,7 +64,8 @@ export default {
     		let that = this
     		console.log("before : " + this.userName)
     		this.$auth.user().updateProfile({
-    			displayName: this.userName
+    			displayName: this.userName,
+                userEmail: this.userEmail
     		}).then(()=> {
     			console.log("promis update" + this.$auth.user().displayName)
 				console.log("after : " + this.userName)
@@ -65,6 +73,9 @@ export default {
     			this.error = error.message
     		})
     	}
+    },
+    components:{
+        navbar
     }
 }
 </script>
