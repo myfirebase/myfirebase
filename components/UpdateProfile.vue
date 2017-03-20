6ad0a78eac766ec9e2f62a98de9015d82927a07f
Amrani Houssain <amranidev@gmail.com>
2017-03-20 22:18:02 +0000

    update profile component

--- components/UpdateProfile.vue
+++ components/UpdateProfile.vue
@@ -1,30 +1,41 @@
 <template>
 <div>
 <navbar></navbar>
-	<div class="col-md-8 col-md-offset-2">
-		<div class="push-down"></div>
-		<div class="panel panel-primary">
-			<div class="panel-heading">
-				Update Profile {{userEmail}}
-			</div>
-			<div class="panel-body" align="center">
-			<img :src="profilePicture" alt="Prifile Picture" class = "img-circle img-responsiv img-rounded img-thumbnail">
-			<div class="push-down"></div>
-				<input type="text" class = "form-control" placeholder="email" :value ="userEmail">
-				<input v-model = "userName" type="text" class = "form-control" placeholder="userName" :value="userName">
-			</div>
-			<div class="panel-footer">
-				<button class="btn btn-primary" @click = "updateProfile()">Submit</button>
-				<button class="btn btn-danger">Getback to app</button>
-			</div>
-		</div>
-	</div>
+<div class="container">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="push-down"></div>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                Update Profile {{userEmail}}
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12" align="center">
+                        <img :src="profilePicture" alt="Prifile Picture" class = "img-circle img-responsive img-rounded img-thumbnail">
+                        <br>
+                        <label class = "btn btn-default btn-xs">
+                            <input type="file" class="hidden" @change="getFile">
+                            <i class="fa fa-file"></i>
+                        </label>
+                        <button v-if="newPhoto" class="btn btn-xs btn-primary" @click="updateAvatar()"><i class="fa fa-save"> update</i></button>
+                    </div>
+                    <div class="col-md-8 col-sm-12">
+                        <input type="text" class = "form-control" placeholder="email" :value ="userEmail">
+                        <input v-model = "userName" type="text" class = "form-control push-down" placeholder="userName" :value="userName">
+                    </div>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <button class="btn btn-primary" @click = "updateProfile()">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 </template>
 
 
 <script>
-
 import navbar from './../components/partials/Navbar.vue'
 
 export default {
@@ -36,45 +47,91 @@ export default {
             then: (user) => {
                 this.userEmail = this.$auth.user().email
                 this.userName = user.displayName
-				this.$storage.getDownloadURL({
-            		ref: this.$auth.user().photoURL,
-            		result: (url) => {
-            			this.profilePicture = url
-            		},
-            		error: (error) => {
-            			this.error = error.message
-            		}
-            	})
-            	console.log("name : " + this.$auth.user().displayName)            
+                this.$storage.getDownloadURL({
+                    ref: this.$auth.user().photoURL,
+                    result: (url) => {
+                        this.profilePicture = url
+                    },
+                    error: (error) => {
+                        this.error = error.message
+                    }
+                })
+                console.log("name : " + this.$auth.user().displayName)
             },
-            catch: () =>{}
+            catch: () => {}
         })
     },
     data() {
         return {
-            message: "hello",
+            message: "",
             userEmail: '',
             profilePicture: '',
-            userName: ''
+            userName: '',
+            newPhoto: null
         }
     },
     computed: {},
     methods: {
-    	updateProfile(){
-    		let that = this
-    		console.log("before : " + this.userName)
-    		this.$auth.user().updateProfile({
-    			displayName: this.userName,
+        getFile(e) {
+            this.newPhoto = e.target.files[0]
+        },
+        updateAvatar() {
+            if (!this.newPhoto) {
+                return
+            }
+            let name = this.newPhoto.name
+
+            this.$storage.upload({
+                ref: `/images/${name}`,
+                file: this.newPhoto,
+                progress: (snapshot) => {
+
+                },
+                error: (err) => {
+                    this.error = err.message
+                },
+                completed: (fileName) => {
+                    this.updateProfilePicture(name)
+                    this.synchronize()
+                }
+            })
+        },
+        updateProfilePicture(fileName) {
+            this.$auth.updateProfilePicture({
+                ref: `/images/${fileName}`,
+                result: () => {
+                    this.message = "Updated!!"
+                    console.log(this.message)
+                },
+                error: (error) => {
+                    this.error = error.message
+                    console.log(this.error)
+                }
+            })
+        },
+        updateProfile() {
+            this.$auth.user().updateProfile({
+                displayName: this.userName,
                 userEmail: this.userEmail
-    		}).then(()=> {
-    			console.log("promis update" + this.$auth.user().displayName)
-				console.log("after : " + this.userName)
-    		}).catch(error => {
-    			this.error = error.message
-    		})
-    	}
+            }).then(() => {
+                this.message = "Updated"
+            }).catch(error => {
+                this.error = error.message
+            })
+        },
+        synchronize() {
+            this.$storage.getDownloadURL({
+                ref: this.$auth.user().photoURL,
+                result: (url) => {
+                    this.profilePicture = url
+                },
+                error: (error) => {
+                    this.error = error.message
+                }
+            })
+        }
     },
-    components:{
+    components: {
         navbar
     }
 }
