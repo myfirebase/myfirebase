a0e835ab7dc7e1b5eceb8b3fcd8582baac9aa9d1
Amrani Houssain <amranidev@gmail.com>
2017-03-20 22:17:36 +0000

    update login component

--- components/Login.vue
+++ components/Login.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="push-down">
-	<div class="conatiner">
+	<div class="container">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4">
 				<div class="alert alert-danger alert-dismissable" v-if = "error">
