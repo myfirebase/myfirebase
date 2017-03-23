f0002b50ca3324e3bd67294fdab335a899c23f3c
Amrani Houssain <amranidev@gmail.com>
2017-03-23 22:35:51 +0000

    update main.js

--- app/main.js
+++ app/main.js
@@ -10,7 +10,7 @@ import store from './../storage/store';
 
 import VueFire from 'vuefire';
 
-import Myfirebase from './../framework/myfirebase'
+import Myfirebase from 'myfirebase'
 
 Vue.use(VueRouter);
 
