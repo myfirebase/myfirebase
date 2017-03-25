aeae86cf85841afa568a6f996060b8eb49699540
Amrani Houssain <amranidev@gmail.com>
2017-03-25 23:44:42 +0000

    update routes

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,23 @@
 const routes = [{
     path: '/',
     component: require('../components/Landing.vue'),
-    name: 'DefaultLayout'
-}, {
-    path: '/login',
-    component: require('../components/Login.vue'),
-    name: 'login'
-}, {
-    path: '/app',
-    component: require('../components/App.vue'),
-    name: 'Main app'
-}, {
-    path: '/update-profile',
-    component: require('../components/UpdateProfile.vue'),
-    name: 'Profile Update'
+    children: [{
+        path: '/',
+        component: require('../components/Welcome.vue'),
+        name: 'Welcome'
+    }, {
+        path: '/login',
+        component: require('../components/Login.vue'),
+        name: 'login'
+    }, {
+        path: '/update-profile',
+        component: require('../components/UpdateProfile.vue'),
+        name: 'Profile Update'
+    }, {
+        path: '/app',
+        component: require('../components/App.vue'),
+        name: 'Main app'
+    }]
 }, {
     path: '*',
     components: require('../components/errors/404.vue')
