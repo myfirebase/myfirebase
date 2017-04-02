4f0d88b8dbd8938e0ef93fda0949e292bf593a47
Amrani Houssain <amranidev@gmail.com>
2017-04-02 20:56:37 +0100

    add github and twitter button

--- components/Login.vue
+++ components/Login.vue
@@ -1,48 +1,50 @@
 <template>
 <div class="push-down">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 col-md-offset-4">
-				<div class="alert alert-danger alert-dismissable" v-if = "error">
-					<a href="#" class="close" @click="error = ''">&times;</a>
-					<strong>{{error}}</strong>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="alert alert-danger alert-dismissable" v-if = "error">
+                    <a href="#" class="close" @click="error = ''">&times;</a>
+                    <strong>{{error}}</strong>
                     <br><br><br>
-				</div>
-				<div class="push-down"></div>
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						login | register
-					</div>
-					<div class="panel-body">
-						<form>
-							<div class="form-group">
-								<input type="email" class = "form-control" placeholder="Email" v-model ="email">
-							</div>
-							<div class="form-group">
-								<input type="password" class = "form-control" placeholder="Password" v-model="password" v-on:keyup.enter= "login()">
-							</div>
-						</form>
-					</div>
-					<div class="panel-footer">
-						<div class="row login-btn-row">
-							<button class="btn btn-default pull-left" @click = "login()" :disabled="loginState">Login <i class="fa fa-sign-in" aria-hidden="true"></i></button>
-							<button class="btn btn-success pull-right" @click = "register" :disabled="loginState" >Register <i class="fa fa-user-plus"></i></button>
-						</div>
-					</div>
-				</div>
-                <div class="push-down">
-                    <button class = 'btn btn-danger' @click = "signInGoogle()">Google <i class="fa fa-google"></i></button>
-                    <button class="btn facebook-color" @click = "signInFacebook()">Facebook <i class="fa fa-facebook"></i></button>
-                    <button class="btn btn-primary">Twitter <i class="fa fa-twitter"></i></button>
                 </div>
-			</div>
-		</div>
-	</div>
+                <div class="push-down"></div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        login | register
+                    </div>
+                    <div class="panel-body">
+                        <form>
+                            <div class="form-group">
+                                <input type="email" class = "form-control" placeholder="Email" v-model ="email">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class = "form-control" placeholder="Password" v-model="password" v-on:keyup.enter= "login()">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="row login-btn-row">
+                            <button class="btn btn-default pull-left" @click = "login()" :disabled="loginState">Login <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+                            <button class="btn btn-success pull-right" @click = "register" :disabled="loginState" >Register <i class="fa fa-user-plus"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="push-down" align="center">
+                    <button class = 'btn btn-danger btn-sm' @click = "signInGoogle()">Google <i class="fa fa-google"></i></button>
+                    <button class="btn facebook-color btn-sm" @click = "signInFacebook()">Facebook <i class="fa fa-facebook"></i></button>
+                    <button class="btn btn-primary btn-sm" @click="signInTwitter()">Twitter <i class="fa fa-twitter"></i></button>
+                    <button class = "btn github-color btn-sm" @click="signInGithub()">Github <i class="fa fa-github"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 <script>
 export default {
     mounted() {
+        this.$store.commit('auth/test')
         this.$auth.logout()
         this.$auth.state({
             forward: '/app',
@@ -56,7 +58,7 @@ export default {
             email: '',
             password: '',
             loginState: false,
-            error: this.$store.state.auth.error,
+            error: '',
         }
     },
     computed: {},
@@ -96,7 +98,7 @@ export default {
             this.$auth.signInWithGoogle({
                 result: (result) => {
                     // This gives you a Google Access Token. You can use it to access the Google API.
-                    //console.log("Token : " + result.credential.accessToken)
+                    // console.log("Token : " + result.credential.accessToken)
                     // The signed-in user info.
                     console.log("User Email : " + result.user.email)
                 },
@@ -108,6 +110,29 @@ export default {
         },
         signInFacebook() {
             this.$auth.signInWithFacebook({
+                result: (result) => {
+                    //console.log("Token : " + result.credential.accessToken)
+                    console.log("User Email : " + result.user.email)
+                },
+                error: (error) => {
+                    console.log("Error : " + error.message)
+                }
+            })
+        },
+        signInTwitter() {
+            this.$auth.signInWithTwitter({
+                result: (result) => {
+                    //console.log("Token : " + result.credential.accessToken)
+                    console.log("User Email : " + result.user.email)
+                    console.log(result.user)
+                },
+                error: (error) => {
+                    console.log("Error : " + error.message)
+                }
+            })
+        },
+        signInGithub() {
+            this.$auth.signInWithGithub({
                 result: (result) => {
                     //console.log("Token : " + result.credential.accessToken)
                     console.log("User Email : " + result.user.email)
