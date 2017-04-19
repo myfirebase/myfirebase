0e47a3e8ce1b23de1125e0e9e18a53a4e0b14bee
Amrani Houssain <amranidev@gmail.com>
2017-04-19 23:13:05 +0100

    update routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,30 +6,38 @@ import UpdateProfile from '@/components/auth/UpdateProfile'
 import App from '@/components/App'
 import NotFound from '@/components/errors/404'
 
-const routes = [{
-    path: '/',
-    component: Landing,
-    children: [{
+const routes = [
+    {
         path: '/',
-        component: Welcome,
-        name: 'Welcome'
-    }, {
-        path: '/login',
-        component: Login,
-        name: 'login'
-    }, {
-        path: '/update-profile',
-        component: UpdateProfile,
-        name: 'Profile Update'
-    }, {
-        path: '/app',
-        component: App,
-        name: 'Main app'
-    }]
-}, {
-    path: '*',
-    components: NotFound
-}]
+        component: Landing,
+        children: [
+            {
+                path: '/',
+                component: Welcome,
+                name: 'Welcome'
+            },
+            {
+                path: '/login',
+                component: Login,
+                name: 'login'
+            },
+            {
+                path: '/update-profile',
+                component: UpdateProfile,
+                name: 'Profile Update'
+            },
+            {
+                path: '/app',
+                component: App,
+                name: 'Main app'
+            }
+        ]
+    },
+    {
+        path: '*',
+        components: NotFound
+    }
+]
 
 
 export default routes;
