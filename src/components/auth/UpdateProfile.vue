fe4ad4a338cd8117f21749fb858587dcdacd03f3
Amrani Houssain <amranidev@gmail.com>
2017-05-16 18:45:39 +0100

    upadate profile component

--- src/components/auth/UpdateProfile.vue
+++ src/components/auth/UpdateProfile.vue
@@ -4,11 +4,11 @@
             <div class="col-md-3 col-sm-12" align="center">
                 <img :src="profilePicture" alt="Prifile Picture" class="img-circle img-responsive img-rounded img-thumbnail image">
                 <div class="push-down">
-                <label class="btn btn-default">
-                    <input type="file" class="hidden" @change="getFile">
-                    Browse <i class="fa fa-file"></i>
-                </label>
-                <button v-if="newPhoto" class="btn btn-primary" @click="updateAvatar()"><i class="fa fa-save"> update</i></button>
+                    <label class="btn btn-default">
+                        <input type="file" class="hidden" @change="getFile">
+                        Browse <i class="fa fa-file"></i>
+                    </label>
+                    <button v-if="newPhoto" class="btn btn-primary" :disabled="uploading" @click="updateAvatar()"><i v-if="uploading" class="fa fa-spinner fa-spin"></i> update</i></button>
                 </div>
             </div>
             <div class="col-md-9 col-sm-12">
@@ -29,94 +29,109 @@
                 <div class="form-group">
                     <input v-model="userName" type="text" class="form-control" placeholder="Username">
                 </div>
-                <a class="btn btn-primary" @click ="updateProfile()">Save Changes</a>
+                <a class="btn btn-primary" @click="updateProfile()" :disabled="saving"><i v-if="saving" class="fa fa-spinner fa-spin"></i> Save Changes</a>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    mounted() {
-        this.$auth.state({
-            forward: '/update-profile',
-            rederict: '/login',
-            then: (user) => {
-                this.userEmail = this.$auth.user().email
-                this.userName = user.displayName
-                this.profilePicture = this.$auth.user().photoURL
-            },
-            catch: () => {}
-        })
-    },
-    data() {
-        return {
-            message: "",
-            userEmail: '',
-            profilePicture: '',
-            userName: '',
-            newPhoto: null,
-            error: ''
-        }
-    },
-    computed: {},
-    methods: {
-        getFile(e) {
-            this.newPhoto = e.target.files[0]
+    export default {
+        mounted() {
+            this.$auth.state({
+                forward: '/update-profile',
+                rederict: '/login',
+                then: (user) => {
+                    this.userEmail = this.$auth.user().email
+                    this.userName = user.displayName
+                    this.profilePicture = this.$auth.user().photoURL
+                },
+                catch: () => {}
+            })
         },
-        updateAvatar() {
-            if (!this.newPhoto) {
-                return
+        data() {
+            return {
+                message: "",
+                userEmail: '',
+                profilePicture: '',
+                userName: '',
+                newPhoto: null,
+                error: '',
+                saving: false,
+                uploading: false,
             }
-            let name = this.newPhoto.name
-            this.$storage.upload({
-                ref: `/images/${name}`,
-                file: this.newPhoto,
-                progress: (snapshot) => {
-                },
-                error: (err) => {
-                    this.error = err.message
-                },
-                completed: (downloadURL) => {
-                    console.log(downloadURL)
-                    this.updateProfilePicture(downloadURL)
-                    this.newPhoto = null
-                }
-            })
         },
-        updateProfilePicture(fileName) {
-            this.$auth.updateProfilePicture({
-                ref: `${fileName}`,
-                result: () => {
-                    this.message = "Updated!!"
-                    this.synchronize()
-                },
-                error: (error) => {
+        computed: {},
+        methods: {
+            getFile(e) {
+                this.newPhoto = e.target.files[0]
+            },
+            updateAvatar() {
+                if (!this.newPhoto) {
+                    return
+                }
+                this.uploading = true
+                let name = this.newPhoto.name
+                this.$storage.upload({
+                    ref: `/images/${name}`,
+                    file: this.newPhoto,
+                    progress: (snapshot) => {},
+                    error: (err) => {
+                        this.error = err.message
+                    },
+                    completed: (downloadURL) => {
+                        console.log(downloadURL)
+                        this.updateProfilePicture(downloadURL)
+                        this.newPhoto = null
+                        this.uploading = false
+                    }
+                })
+            },
+            updateProfilePicture(fileName) {
+                this.$auth.updateProfilePicture({
+                    ref: `${fileName}`,
+                    result: () => {
+                        this.message = "Updated!!"
+                        this.synchronize()
+                    },
+                    error: (error) => {
+                        this.error = error.message
+                        console.log(this.error)
+                    }
+                })
+            },
+            updateProfile() {
+                this.saving = true
+                this.$auth.user().updateProfile({
+                    displayName: this.userName,
+                    email: this.userEmail
+                }).then(() => {
+                    this.$store.state.database.child("data").once("value").then(snapshot => {
+                        snapshot.forEach(childSnapshot => {
+                            if (childSnapshot.child("email").val() == this.$auth.user().email) {
+                                this.$store.state.database.child("data").child(childSnapshot.key).update({
+                                    name: this.userName
+                                })
+                            }
+                        })
+                    })
+                    this.message = "Updated"
+                    this.saving = false
+                }).catch(error => {
                     this.error = error.message
-                    console.log(this.error)
-                }
-            })
-        },
-        updateProfile() {
-            this.$auth.user().updateProfile({
-                displayName: this.userName,
-                email: this.userEmail
-            }).then(() => {
-                this.message = "Updated"
-            }).catch(error => {
-                this.error = error.message
-            })
-        },
-        synchronize() {
-            this.profilePicture = this.$auth.user().photoURL
+                    this.saving = false
+                })
+            },
+            synchronize() {
+                this.profilePicture = this.$auth.user().photoURL
+            }
         }
     }
-}
 </script>
 
 
 <style>
-    .image{
+    .image {
         max-width: 200px;
         min-width: 150px
     }
