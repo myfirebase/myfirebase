5cd325b7bfb3380935dd1647bf1a471f6b7fff50
Amrani Houssain <amranidev@gmail.com>
2017-07-12 17:29:40 +0100

    update navbar

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,76 +1,70 @@
 <template>
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" @click='collapsAction'>
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			</button>
-			<router-link to="/" class = "navbar-brand">{{logo}}</router-link>
-		</div>
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div :class="collaps">
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<router-link to="/" class="navbar-brand">{{logo}}</router-link>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
 			<ul class="nav navbar-nav">
-				<li><router-link to="/">Home</router-link></li>
-				<li><router-link to="/app">App</router-link></li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li class="">
-					<a href="#" role="button" @click = "toggle = !toggle">{{username}} <span class="caret"></span></a>
-					<ul v-if="toggle" class="dropdown-menu">
-						<li v-if = "!signed"><router-link to = "login">Login</router-link></li>
-						<li v-if = "signed"><router-link to = 'update-profile'>Profile</router-link></li>
-						<li role="separator" class="divider"></li>
-						<li v-if = "signed"><a href="/login" @click="logout()">Logout</a></li>
-					</ul>
+				<li>
+					<router-link to="/">Home</router-link>
+				</li>
+				<li>
+					<router-link to="/app">App</router-link>
 				</li>
 			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li v-if="!signed">
+					<router-link to="login">Login</router-link>
+				</li>
+				<li v-if="signed">
+					<router-link to='update-profile'>Profile</router-link>
+				</li>
+				<li role="separator" class="divider"></li>
+				<li v-if="signed"><a href="/login" @click="logout()">Logout</a></li>
+			</ul>
 		</div>
-	</div>
-</nav>
+	</nav>
 </template>
 
 <script>
-export default {
-    mounted() {
-    	this.$auth.state({
-            forward: '/update-profile',
-            redirect: '/login',
-    		then: (user) => {
-    			this.signed = true
-    			this.username = user.displayName
-    		},
-    		catch: () => {
-    			this.username = "Auth"
-    		}
-    	})
-    },
-    data() {
-        return {
-            logo: "Myfirebase",
-            toggle: false,
-            signed: false,
-            username: '',
-            collapsClass: 'collapse navbar-collapse'
-        }
-    },
-    computed:{
-    	collaps()
-    	{
-    		return this.collapsClass
-    	}
-    },
-    methods:{
-    	logout(){
-    		this.$auth.logout()
-    		this.signed = false
-    	},
-    	collapsAction(){
-    		this.collapsClass = this.collapsClass ? '' : 'collapse navbar-collapse'
-    	}
-    }
-}
+	export default {
+		mounted() {
+			this.$auth.state({
+				forward: '/update-profile',
+				redirect: '/login',
+				then: (user) => {
+					this.signed = true
+					this.username = user.displayName
+				},
+				catch: () => {
+					this.username = "Auth"
+				}
+			})
+		},
+		data() {
+			return {
+				logo: "Myfirebase",
+				toggle: false,
+				signed: false,
+				username: '',
+				collapsClass: 'collapse navbar-collapse'
+			}
+		},
+		computed: {
+			collaps() {
+				return this.collapsClass
+			}
+		},
+		methods: {
+			logout() {
+				this.$auth.logout()
+				this.signed = false
+			},
+			collapsAction() {
+				this.collapsClass = this.collapsClass ? '' : 'collapse navbar-collapse'
+			}
+		}
+	}
 </script>
