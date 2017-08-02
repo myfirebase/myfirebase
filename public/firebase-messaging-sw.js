9320b97730fdb86cafae74a615d88d9a1fda59bd
Amrani Houssain <amranidev@gmail.com>
2017-08-02 18:11:32 +0100

    update firebase-messaging sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,14 +14,5 @@ const messaging = firebase.messaging();
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
     // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
 });
