20e9de0575340371f3cf86b6e969f853bd0f5005
Amrani Houssain <amranidev@gmail.com>
2017-08-10 13:18:34 +0100

    update tests.spec.js

--- tests/unit/specs/test.spec.js
+++ tests/unit/specs/test.spec.js
@@ -6,6 +6,6 @@ describe('Hello.vue', () => {
         const Constructor = Vue.extend(Hello)
         const vm = new Constructor().$mount()
         expect(vm.$el.querySelector('.hello h1').textContent)
-            .to.equal('Welcome to Your Vue.js PWA')
+            .to.equal('Welcome to Your Vue.js App')
     })
 })
