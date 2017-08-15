5fe5f301069b92153f1ed3b7ad003dd865beaeb4
Amrani Houssain <amranidev@gmail.com>
2017-08-15 17:23:32 +0100

    update webpack base conf

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -16,8 +16,7 @@ module.exports = {
         path: config.build.assetsRoot,
         filename: '[name].js',
         publicPath: process.env.NODE_ENV === 'production' ?
-            config.build.assetsPublicPath :
-            config.dev.assetsPublicPath
+            config.build.assetsPublicPath : config.dev.assetsPublicPath
     },
     resolve: {
         extensions: ['.js', '.vue', '.json'],
