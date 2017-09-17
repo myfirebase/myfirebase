1433e18c0874235131df1cc5f78bdd0b49cfd00d
Amrani Houssain <amranidev@gmail.com>
2017-09-17 17:49:59 +0100

    opt

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,6 @@
 // Firebase app configuration.
 const config = {
-    apiKey: "AIzaSyBjz_xr7I9K64mz72ota508Jh8KAte9Qhk",
-    authDomain: "test-58233.firebaseapp.com",
-    databaseURL: "https://test-58233.firebaseio.com",
-    projectId: "test-58233",
-    storageBucket: "test-58233.appspot.com",
-    messagingSenderId: "754134907775"
+    // Past your configuration here.
 }
 
 export default config;
