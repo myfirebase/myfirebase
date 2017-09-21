96fb965d4c4d9304be37546de0fa411d99d7f2a4
Amrani Houssain <amranidev@gmail.com>
2017-09-21 19:17:38 +0100

    init middlewares

---
+++ src/middlewares/index.js
@@ -0,0 +1,12 @@
+/**
+ * Here where you can register your middlewares
+ * you can simply do that by adding them to the middlrwares array.
+ */
+
+// import AuthMiddleware.
+import AuthMiddleware from './auth'
+
+// register AuthMiddleware.
+const middlewares = [AuthMiddleware]
+
+export default middlewares
