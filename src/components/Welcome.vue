c195727fe23abb39f40cf35a0b2002024bec2279
Amrani Houssain <amranidev@gmail.com>
2017-10-12 18:17:00 +0100

    update Welcome.vue

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,6 +2,7 @@
 <div class="flex">
     <img src="./../assets/img/MyFireBase.svg" alt="MyfirebaseLogo">
     <span class="md-display-2">Welcome to <i>Myfirebase</i></span>
+    <router-link tag="md-button" to="/login" class="md-raised md-primary">Login</router-link>
 </div>
 </template>
 <script>
