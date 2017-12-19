1722e24c4ddf8484236d212cd4b27d2257db90ba
Amrani Houssain <amranidev@gmail.com>
2017-12-19 14:32:16 +0000

    typo

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
  */
 require('@/assets/sass/app.scss')
 
-//The Vue build version to load with the `import` command
+// The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import router from '@/router'
