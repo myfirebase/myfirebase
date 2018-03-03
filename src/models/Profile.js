2970b2d2f396edeaefe3a29e627a974353262e4a
Houssain Amrani <amranidev@gmail.com>
2018-03-03 17:07:16 +0000

    update Profile.js

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -1,30 +1,57 @@
 export default class Profile {
     
+    /** 
+     * Create new Profile Instance. 
+     */
     constructor() {
         this.email = ""
         this.displayName = ""
         this.photoURL = ""
     }
 
+    /**
+     * Set Email.
+     * 
+     * @param {string} email
+     * 
+     * @return Profile 
+     */
     setEmail(email) {
         this.email = email
         return this
     }
 
+    /**
+     * Set displayName.
+     * 
+     * @param {string} displayName
+     * 
+     * @return Profile
+     */
     setDisplayName(displayName) {
         this.displayName = displayName
         return this
     }
 
+    /**
+     * Set photoURL.
+     * 
+     * @param {string} photoURL
+     * 
+     * @return Profile 
+     */
     setPhotoURL(photoURL) {
         this.photoURL = photoURL 
         return this
     }
 
+    /** 
+     * Export to Json. 
+     */
     toJson() {
         return {
             email: this.email,
-            displayName: this.displayName
+            displayName: this.displayName,
         }
     }
 }
