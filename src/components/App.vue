d9c075a5fdf99a3611a47ac458746055ab482109
Houssain Amrani <amranidev@gmail.com>
2018-03-15 23:37:41 +0000

    update App.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -9,20 +9,20 @@
                 <md-whiteframe class="data-container">
                     <md-list>
                         <md-subheader>Data Example</md-subheader>
-                        <md-list-item v-for="snapshot in data" :key="snapshot.data">
+                        <md-list-item v-for="item in data" :key="item['.key']">
                             <span>
-                                {{snapshot.data}}
+                                {{item.name}}
                             </span>
-                            <md-button @click.native="deleteData(snapshot['.key'])" class="md-icon-button md-list-action">
+                            <md-button @click.native="deleteItem(item['.key'])" class="md-icon-button md-list-action">
                                 <md-icon class="md-accent">delete</md-icon>
                             </md-button>
                         </md-list-item>
                         <md-list-item>
                             <md-input-container>
-                                <label>Write</label>
-                                <md-input v-model="newData" v-on:keyup.enter.native="addData()" placeholder="Write something..."></md-input>
+                                <label>Add Item</label>
+                                <md-input v-model="Item.name" v-on:keyup.enter.native="addItem()" placeholder="Add Item..."></md-input>
                             </md-input-container>
-                            <md-button class="md-icon-button md-list-action" @click.native="addData()">
+                            <md-button class="md-icon-button md-list-action" @click.native="addItem()">
                                 <md-icon class="md-primary">send</md-icon>
                             </md-button>
                         </md-list-item>
@@ -34,6 +34,9 @@
 </template>
 
 <script>
+
+import Item from "./../models/Item";
+
 export default {
   mounted() {
     this.$auth.check({
@@ -43,7 +46,7 @@ export default {
       catch(error) {}
     });
     // retrieve messaging token.
-    // only for production, you have to register messaging serviceworker.
+    // only for production, you have to register serviceworker.
     /*
         this.$store.state.messaging.getToken()
             .then((token) => {
@@ -58,25 +61,20 @@ export default {
   },
   data() {
     return {
-      message: "welcome",
-      newData: "",
       error: "",
       token: "",
-      userEmail: ""
-    };
+      userEmail: "",
+      Item: new Item(this.$store.state.database.child('data')).init()
+    }
   },
   methods: {
-    addData() {
+    addItem() {
       this.$firebaseRefs.data.onDisconnect().cancel();
-      this.$firebaseRefs.data.push({
-        data: this.newData,
-        name: this.userEmail,
-        token: this.token
-      });
-      this.newData = "";
+      this.Item.push();
+      this.Item.name = ""
     },
-    deleteData(key) {
-      this.$firebaseRefs.data.child(key).remove();
+    deleteItem(key) {
+      this.Item.remove(key);
     }
   }
 };
