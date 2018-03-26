86103ddcbb0da42a2b88930a0b0f2d2fdc00098a
Amrani Houssain <amranidev@users.noreply.github.com>
2018-03-26 23:16:23 +0100

    Update Person.js

--- src/models/Person.js
+++ src/models/Person.js
@@ -1,10 +1,21 @@
 import { FirestoreModel } from 'myfirebase'
 
 class Person extends FirestoreModel {
+
+    /**
+     * Create new Person instance.
+     * 
+     * @param {*} ref
+     */
     constructor(ref) {
         super(ref)
     }
 
+    /**
+     * Define required properties.
+     * 
+     * @return array
+     */
     required() {
         return []
     }
