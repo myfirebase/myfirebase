0aeb3aab55bedcf11bd9b1283562cbf3289c7578
Amrani Houssain <amranidev@users.noreply.github.com>
2018-03-26 23:15:16 +0100

    Update Item.js

--- src/models/Item.js
+++ src/models/Item.js
@@ -1,10 +1,21 @@
 import { DatabaseModel } from 'myfirebase'
 
 class Item extends DatabaseModel {
+
+    /**
+     * Create new Item instance.
+     * 
+     * @param {*} ref
+     */
     constructor(ref) {
         super(ref)
     }
 
+    /**
+     * Define required properties.
+     * 
+     * @return array
+     */
     required() {
         return []
     }
