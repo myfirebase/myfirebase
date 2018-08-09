c95b5845d632bfcc50d71e240b106c4927247a07
Amrani Houssain <houssain@airbit.com>
2018-08-09 21:53:17 +0100

    middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,23 +3,23 @@
  * to myfirebase functionalities and vue auth guard via actions.
  * myfirebase => [auth, storage, database, firestore]
  * actions => [to, from, next()]
- * 
- * @param {object} myfirebase 
- * @param {object} actions 
+ *
+ * @param {object} myfirebase
+ * @param {object} actions
  */
 const AuthMiddleware = (myfirebase, actions) => {
-    if (actions.to.matched.some(record => record.meta.auth)) {
-        myfirebase.auth.check({
-            then: (user) => {
-                actions.next()
-            },
-            catch: () => {
-                actions.next({ path: '/login' })
-            }
-        })
-    } else {
-        actions.next()
-    }
-}
+  if (actions.to.matched.some(record => record.meta.auth)) {
+    myfirebase.auth
+      .check()
+      .then(user => {
+        actions.next();
+      })
+      .catch(err => {
+        actions.next({ path: '/login' });
+      });
+  } else {
+    actions.next();
+  }
+};
 
-export default AuthMiddleware
+export default AuthMiddleware;
