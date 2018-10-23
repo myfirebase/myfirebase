ec487c1f4aa67d298c56d23c680bd33b8b1af8c6
Amrani Houssain <amranidev@gmail.com>
2018-10-23 22:46:08 +0100

    update index

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     mode: 'history',
     base: __dirname,
     routes: routes,
-    scrollBehavior(to, from, savedPosition) {
+    scrollBehavior (to, from, savedPosition) {
         return {
             x: 0,
             y: 0
