88038464a92cc6326010ffbe66986917def31a64
Amrani Houssain <amranidev@gmail.com>
2018-11-02 21:53:12 +0000

    update tests

--- tests/unit/specs/test.spec.js
+++ tests/unit/specs/test.spec.js
@@ -2,15 +2,6 @@ import Vue from 'vue'
 import Hello from '@/components/Hello'
 import UpdateProfile from '@/components/auth/UpdateProfile'
 
-describe('Hello.vue', () => {
-    it('should render correct contents', () => {
-        const Constructor = Vue.extend(Hello)
-        const vm = new Constructor().$mount()
-        expect(vm.$el.querySelector('.hello h1').textContent)
-            .to.equal('Welcome to Your Vue.js App')
-    })
-})
-
 describe('UpdateProfile.vue', () => {
     it('type of sub-head should be string', () => {
         const Constructor = Vue.extend(UpdateProfile)
