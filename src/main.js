f19270f3e8082d4b75f96d3d5807af84aadf3023
Houssain Amrani <amranidev@users.noreply.github.com>
2018-12-24 21:22:08 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@
  */
 require('@/assets/sass/app.scss')
 
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import router from '@/router'
 import store from './../storage/store'
@@ -23,7 +21,7 @@ import middlewares from '@/middlewares/index'
 
 Vue.config.productionTip = false
 
-// Init VueFire (Firebase real time database)
+// Init VueFire (Firebase realtime database)
 Vue.use(VueFire)
 Vue.use(Firestore)
 
